877d789e03333be1d50594150d914e1d5105e76a
fuzhenghao <1045612802@qq.com>
2021-09-26 18:23:59 +0800

    umi项目框架搭建

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# myBlog
-个人博客
+# umi project
+
+## Getting Started
+
+Install dependencies,
+
+```bash
+$ yarn
+```
+
+Start the dev server,
+
+```bash
+$ yarn start
+```
---
+++ src/pages/index.tsx
@@ -0,0 +1,19 @@
+/*
+ * @Author: fuzhenghao
+ * @Date: 2021-09-26 11:36:59
+ * @LastEditTime: 2021-09-26 18:03:56
+ * @LastEditors: fuzhenghao
+ * @Description:
+ * @FilePath: \myBlog_frontEnd\src\pages\index.tsx
+ *
+ */
+import styles from './index.less';
+
+export default function IndexPage() {
+  return (
+    <div className={styles.index_page}>
+      <div className={styles.page_1}>page_1</div>
+      <div className={styles.page_2}>page_2</div>
+    </div>
+  );
+}
